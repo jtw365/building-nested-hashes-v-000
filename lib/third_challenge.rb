acd01dcf01eb8df9894623c32d39aa0187f6a49c
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-26 09:20:55 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,8 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "Lord Montague"
+        :name => "Lord Montague",
+        :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
@@ -12,7 +13,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -27,5 +28,5 @@ def third_challenge
    }
   }
 
-  
+
 end
