b2019e64dcdf317347a45b4d9d741b2caef4ef20
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-26 09:25:07 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -12,8 +12,8 @@ def third_challenge
         #your key/value pairs here
         },
       :hero => {
-        :name => "Romeo"
-        :age => "15"
+        :name => "Romeo",
+        :age => "15",
         :status => "alive"
         #your key/value pairs here
         },
@@ -21,18 +21,18 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        :name => "Lord Capulet"
+        :name => "Lord Capulet",
         :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
-        :name => "Lady Capulet"
+        :name => "Lady Capulet",
         :age => "51"
         #your key/value pairs here
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
+        :name => "Juliet",
+        :age => "15",
         :status => "alive"
         #your key/value pairs here
         },
